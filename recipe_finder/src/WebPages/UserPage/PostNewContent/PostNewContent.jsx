a433a67f683ed2e315bb3e0c8fc543532cec0c20
tonglin <tl82003@gmail.com>
2023-06-27 20:16:23 -0400

    changed the add new form

--- recipe_finder/src/WebPages/UserPage/PostNewContent/PostNewContent.jsx
+++ recipe_finder/src/WebPages/UserPage/PostNewContent/PostNewContent.jsx
@@ -1,127 +1,132 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import { MDBInput, MDBTextArea } from 'mdb-react-ui-kit';
 import { useNavigate } from "react-router-dom";
 import "./PostNewContent.css";
-import '../../../components/common/Button-74.css'
+import '../../../components/common/Button-74.css';
 
-//addNewContent: add new content to the database, this page has get the data without crossing any other files
-// post new content to the database, the form is located here
-export default function PostNewContent( {addNewContent} )
-{
-    const [postType, setPostType] = useState('');
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
-    const [tag, setTags] = useState('');
-    const [imgUrl, setImgUrl] = useState('');
+export default function PostNewContent({ addNewContent }) {
+  const [formContent, setFormContent] = useState({
+    postType: "",
+    title: "",
+    content: "",
+    tag: "",
+    imgUrl: ""
+  });
 
-    const navigate = useNavigate();
+  const { postType, title, content, tag, imgUrl } = formContent;
 
-    const handleNewPostSubmit = (event) => {
-        event.preventDefault();
-        // content won't be added into the list for now, but will in the future development
-        if (postType === "recipe")
-        {
-            const newContent = {
-                "title": title, 
-                "imgUrl": imgUrl,
-                "rate": 0,
-                "popularity": 0,
-                "tag": tag
-            }
-            alert("I got the request");
-            navigate('/');
-            addNewContent(newContent);
-        }
-        else
-        {
-            alert("Sorry, the blog section is currently unavailable.");
-            navigate('/');
-        }
-    };
+  const handleOnChange = (event) => {
+    setFormContent((formContent) => ({
+      ...formContent,
+      [event.target.name]: event.target.value
+    }));
+  };
 
+  const navigate = useNavigate();
 
-    return(
-        <>
-        <div className="newPostForm">
-            <h1>Post new content</h1>
-            <h3>Let more people explore about your furry friend</h3>
+  const handleNewPostSubmit = (event) => {
+    event.preventDefault();
+    if (postType === "recipe") {
+      alert("I got the request");
+      navigate('/');
+      addNewContent(formContent);
+    } else {
+      alert("Sorry, the blog section is currently unavailable.");
+      navigate('/');
+    }
+  };
 
-            <form onSubmit={handleNewPostSubmit}>
-                <div className="postContentInput my-4">
-                    <label>Post Type:</label>
-                    <div>
-                        <label className="mx-2">
-                            <input
-                            type="radio"
-                            value="recipe"
-                            checked={postType === 'recipe'}
-                            onChange={(event) => setPostType(event.target.value)}
-                            />
-                            Recipe
-                        </label>
-                        <label>
-                            <input
-                            type="radio"
-                            value="blog"
-                            checked={postType === 'blog'}
-                            onChange={(event) => setPostType(event.target.value)}
-                            />
-                            Blog
-                        </label>
-                    </div>
-                </div>
-                <div className="postContentInput my-4">
-                    <MDBInput label='Title' 
-                    type='text' 
-                    value={title} 
-                    onChange={(event) => setTitle(event.target.value)}
-                    />
-                </div>
-                <div className="postContentInput my-4">
-                    <MDBInput type="text" 
-                    label="Image Url"
-                    onChange={(event) => setImgUrl(event.target.value)}
-                    value = {imgUrl}
-                    />
-                </div>
-                <div className="postContentInput my-4">
-                    <MDBTextArea 
-                    label='Content' 
-                    value={content}
-                    onChange={(event) => setContent(event.target.value)}
-                    rows={4} />
-                </div>
-                <div className="postContentInput my-4">
-                <div>
-                    <label>Tag: </label>
-                        <label className="mx-2">
-                            <input
-                            type="radio"
-                            value="dog"
-                            checked={tag === 'dog'}
-                            onChange={(event) => setTags(event.target.value)}
-                            />
-                            Dog
-                        </label>
-                        <label>
-                            <input
-                            type="radio"
-                            value="cat"
-                            checked={tag === 'cat'}
-                            onChange={(event) => setTags(event.target.value)}
-                            />
-                            Cat
-                        </label>
-                    </div>
-                </div>
-                <div className="d-flex justify-content-center">
-                    <button type="submit" className="button-74" >
-                        Submit
-                    </button>
-                </div>
-            </form>
-        </div>
-        </>
-    );
+  return (
+    <>
+      <div className="newPostForm">
+        <h1>Post new content</h1>
+        <h3>Let more people explore about your furry friend</h3>
+
+        <form onSubmit={handleNewPostSubmit}>
+          <div className="postContentInput my-4">
+            <label>Post Type:</label>
+            <div>
+              <label className="mx-2">
+                <input
+                  type="radio"
+                  value="recipe"
+                  name="postType"
+                  checked={postType === 'recipe'}
+                  onChange={handleOnChange}
+                />
+                Recipe
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  value="blog"
+                  name="postType"
+                  checked={postType === 'blog'}
+                  onChange={handleOnChange}
+                />
+                Blog
+              </label>
+            </div>
+          </div>
+          <div className="postContentInput my-4">
+            <MDBInput
+              label='Title'
+              value={title}
+              type='text'
+              name='title'
+              onChange={handleOnChange}
+            />
+          </div>
+          <div className="postContentInput my-4">
+            <MDBInput
+              type="text"
+              label="Image Url"
+              name = 'imgUrl'
+              onChange={handleOnChange}
+              value={imgUrl}
+            />
+          </div>
+          <div className="postContentInput my-4">
+            <MDBTextArea
+              label='Content'
+              value={content}
+              name='content'
+              onChange={handleOnChange}
+              rows={4}
+            />
+          </div>
+          <div className="postContentInput my-4">
+            <div>
+              <label>Tag: </label>
+              <label className="mx-2">
+                <input
+                  type="radio"
+                  value="dog"
+                  name="tag"
+                  checked={tag === 'dog'}
+                  onChange={handleOnChange}
+                />
+                Dog
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  value="cat"
+                  name="tag"
+                  checked={tag === 'cat'}
+                  onChange={handleOnChange}
+                />
+                Cat
+              </label>
+            </div>
+          </div>
+          <div className="d-flex justify-content-center">
+            <button type="submit" className="button-74" >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
